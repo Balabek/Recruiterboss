b7b3c295cbb81c1cbf162116fcc768e76fdb3e47
Balabek <vonbabayan@gmail.com>
2017-09-19 16:15:24 +0300

    Recruiterboss (FINAL)

--- js/recruiterboss.js
+++ js/recruiterboss.js
@@ -1,3 +1,19 @@
 /**
  * Created by krm on 05/09/2017.
  */
+
+$(document).ready(function () {
+
+    // 'Proceed to Payment' button's freeze/unfreeze behavior on 'Pricing Step 2' page
+    function triggerChange(){
+        $("#billingTermsAgreementCheck").trigger("change");
+    }
+
+    $("#billingTermsAgreementCheck").change(function() {
+        $('#proceedToPayment').toggleClass('frozen');
+    });
+
+    triggerChange();
+
+
+});
